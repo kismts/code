9bf938eb11cdf0676c3ee735ae9497546a9357a1
kismts <kismts@users.noreply.github.com>
2025-06-06 16:07:32 +0200

    vmi

--- src/ch06/util.py
+++ src/ch06/util.py
@@ -2,12 +2,19 @@ import numpy as np
 
 #from ch05.util import *
 
+def oracle(state, predicate):
+    for item in range(len(state)):
+        if predicate(item):
+            state[item] *= -1
+
 
 def random_transformation(n):
     import scipy.stats
     U = scipy.stats.unitary_group.rvs(2**n)
+    print("\nA matrix:")
     print(U)
     UT = np.conj(U.transpose())
+    print("\nA-1 matrix:")
     print(UT)
 
     def f_direct(state):
@@ -24,9 +31,53 @@ def random_transformation(n):
 
     return f_direct, f_inverse
 
+def init_state(n):
+    state = [0 for _ in range(2**n)]
+    state[0] = 1
+    return state
 
-def inner(v1, v2):
-    assert(len(v1) == len(v2))
-    return sum(z1*z2.conjugate() for z1, z2 in zip(v1, v2))
+from math import log2
+def inversion_0_transformation(f, state):
+    n = int(log2(len(state)))
 
-random_transformation(2)
+    transform = f[0]
+    inverse_transform = f[1]
+
+    inverse_transform(state)
+    print("\nstate after inv_A:")
+    print(state)
+
+    #assert is_close(state[0].imag, 0)
+    for k in range(1, len(state)):
+        state[k] = -state[k]
+    print("\nstate after M0:")
+    print(state)
+
+    transform(state)
+    print("\nstate after A matrix (second):")
+    print(state)
+
+
+n = 2
+f = random_transformation(n)
+A = f[0]
+
+state = init_state(n)
+print("\ninit state:")
+print(state)
+
+A(state)
+print("\nstate after A matrix (first):")
+print(state)
+
+predicate = lambda k: True if k == 3 else False
+oracle(state, predicate)
+print("\nstate after oracle:")
+print(state)
+
+inversion_0_transformation(f, state)
+
+
+
+
+

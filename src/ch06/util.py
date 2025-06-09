77a3487516aae59d6c2f758f5ffc1546d21eeb5a
kismts <kismts@users.noreply.github.com>
2025-06-09 18:24:14 +0200

    u

--- src/ch06/util.py
+++ src/ch06/util.py
@@ -91,25 +91,6 @@ def classical_grover(state, predicate, iterations):
         #p = sum([abs(state[k])**2 for k in items])
         #assert is_close(p, sin((2 * it + 1)*theta)**2) 
 
-#from ch04.util import *
-
-from math import sqrt, cos, sin, pi
-
-import random
-
-
-def generate_state(n, seed=555):
-    # Choose a seed
-    random.seed(seed)
-    # Generate 4 random probabilities that add up to 1
-    probs = [random.random() for _ in range(2 ** n)]
-    total = sum(probs)
-    probs = [p / total for p in probs]
-    # Generate 4 random angles in radians
-    angles = [random.uniform(0, 2 * pi) for _ in range(2 ** n)]
-    # Build the quantum state array
-    return [sqrt(p) * (cos(a) + 1j * sin(a)) for (p, a) in zip(probs, angles)]
-
 from math import log2
 def inversion_0_transformation(f, state):
     n = int(log2(len(state)))
@@ -137,7 +118,6 @@ print("\ninit state:")
 print(state)
 
 A(state)
-state = generate_state(3)
 print_state("state after A matrix:", state)
 copy = state.copy()
 
